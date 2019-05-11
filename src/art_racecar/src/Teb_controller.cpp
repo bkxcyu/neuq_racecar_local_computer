d5f7ccc214f923242d3ce8d0cb94c07cb10ede6f
bkxcyu <1335823722@qq.com>
2019-05-11 14:32:13 +1200

    add dependend and head file of teb&rsband successfully

--- src/art_racecar/src/Teb_controller.cpp
+++ src/art_racecar/src/Teb_controller.cpp
@@ -7,6 +7,9 @@
 #include "nav_msgs/Path.h"
 #include <nav_msgs/Odometry.h>
 #include <visualization_msgs/Marker.h>
+#include <teb_local_planner/FeedbackMsg.h>
+#include <teb_local_planner/teb_local_planner_ros.h>
+#include "rsband_local_planner/rsband_local_planner_ros.h"
 
 class Teb_controller
 {
@@ -18,22 +21,36 @@ private:
     /* data */
     ros::NodeHandle n_;
     ros::Subscriber odom_sub, path_sub, goal_sub;
+    ros::Publisher pub_;
+    ros::Timer timer1, timer2;
     nav_msgs::Odometry odom;
     nav_msgs::Path map_path;
-
+    geometry_msgs::Point odom_goal_pos;
+    bool goal_received;
+    bool goal_reached;
+    tf::TransformListener tf_listener;
+    int controller_freq;
 
     void odomCB(const nav_msgs::Odometry::ConstPtr& odomMsg);
     void pathCB(const nav_msgs::Path::ConstPtr& pathMsg);
     void goalCB(const geometry_msgs::PoseStamped::ConstPtr& goalMsg);
+    void goalReachingCB(const ros::TimerEvent&);
+    void controlLoopCB(const ros::TimerEvent&);
 };
 
 Teb_controller::Teb_controller()
 {
     ros::NodeHandle p("~");
 
+    p.param("controller_freq", controller_freq, 20);//控制频率
+
     odom_sub=n_.subscribe("/odometry/filtered",1,&Teb_controller::odomCB,this);
     path_sub=n_.subscribe("/planed_path",1,&Teb_controller::pathCB,this);
     goal_sub=n_.subscribe("move_base_simple/goal",1,&Teb_controller::goalCB,this);
+    pub_=n_.advertise<geometry_msgs::Twist>("car/cmd_vel",1);
+
+    timer1 = n_.createTimer(ros::Duration((1.0)/controller_freq), &Teb_controller::controlLoopCB, this); // Duration(0.05) -> 20Hz//根据实时位置信息和导航堆栈更新舵机角度和电机速度，存在cmd_vel话题里
+    timer2 = n_.createTimer(ros::Duration((0.5)/controller_freq), &Teb_controller::goalReachingCB, this); // Duration(0.05) -> 20Hz//判断是否到达目标位置
 
 }
 
@@ -49,6 +66,20 @@ void Teb_controller::pathCB(const nav_msgs::Path::ConstPtr& pathMsg)
 
 void Teb_controller::goalCB(const geometry_msgs::PoseStamped::ConstPtr& goalMsg)
 {
+    geometry_msgs::PoseStamped odom_goal;
+    tf_listener.transformPose("odom", ros::Time(0) , *goalMsg, "map" ,odom_goal);
+    odom_goal_pos = odom_goal.pose.position;
+    goal_received = true;
+    goal_reached = false;
+}
+
+void Teb_controller::controlLoopCB(const ros::TimerEvent&)
+{
+
+}
+
+void Teb_controller::goalReachingCB(const ros::TimerEvent&)
+{
 
 }
 
@@ -56,7 +87,7 @@ int main(int argc, char **argv)
 {
     /* code for main function */
     ros::init(argc, argv, "Teb_Controller");
-    Teb_controller Controller;
+    Teb_controller controller;
     ros::spin();
     return 0;
 }
